fe5baef386fb29c7be5fb96fd35fe6a8606ca56b
Félix Díez <felixeu31@gmail.com>
2024-02-02 17:21:12 +0100

    refactor: rename variables

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -10,43 +10,43 @@ namespace TheatricalPlayersRefactoringKata
         {
             var totalAmount = 0;
             var volumeCredits = 0;
-            var result = string.Format("Statement for {0}\n", invoice.Customer);
+            var statement = $"Statement for {invoice.Customer}\n";
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
-            foreach(var perf in invoice.Performances) 
+            foreach(var performance in invoice.Performances) 
             {
-                var play = plays[perf.PlayID];
-                var thisAmount = 0;
+                var play = plays[performance.PlayID];
+                var performanceAmount = 0;
                 switch (play.Type) 
                 {
                     case "tragedy":
-                        thisAmount = 40000;
-                        if (perf.Audience > 30) {
-                            thisAmount += 1000 * (perf.Audience - 30);
+                        performanceAmount = 40000;
+                        if (performance.Audience > 30) {
+                            performanceAmount += 1000 * (performance.Audience - 30);
                         }
                         break;
                     case "comedy":
-                        thisAmount = 30000;
-                        if (perf.Audience > 20) {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
+                        performanceAmount = 30000;
+                        if (performance.Audience > 20) {
+                            performanceAmount += 10000 + 500 * (performance.Audience - 20);
                         }
-                        thisAmount += 300 * perf.Audience;
+                        performanceAmount += 300 * performance.Audience;
                         break;
                     default:
                         throw new Exception("unknown type: " + play.Type);
                 }
                 // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                volumeCredits += Math.Max(performance.Audience - 30, 0);
                 // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)performance.Audience / 5);
 
                 // print line for this order
-                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-                totalAmount += thisAmount;
+                statement += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(performanceAmount / 100), performance.Audience);
+                totalAmount += performanceAmount;
             }
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-            result += String.Format("You earned {0} credits\n", volumeCredits);
-            return result;
+            statement += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+            statement += String.Format("You earned {0} credits\n", volumeCredits);
+            return statement;
         }
     }
 }
