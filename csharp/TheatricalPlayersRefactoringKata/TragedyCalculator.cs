3a282708c83f77047e18373eb6b1aa562e129db5
Félix Díez <felixeu31@gmail.com>
2024-02-14 11:13:22 +0100

    refactor: simplify logic of tragedy calculator (subtitute query with method, extract field, ...)

--- csharp/TheatricalPlayersRefactoringKata/TragedyCalculator.cs
+++ csharp/TheatricalPlayersRefactoringKata/TragedyCalculator.cs
@@ -5,6 +5,9 @@ namespace TheatricalPlayersRefactoringKata;
 public class TragedyCalculator : IPerformanceCalculator
 {
     private readonly Performance _performance;
+    private readonly int _volumeThreshold = 30;
+    private readonly decimal _baseAmount = 400;
+    private readonly int _extraAmountPerExceedAssistant = 10;
 
     public TragedyCalculator(Performance performance)
     {
@@ -12,12 +15,21 @@ public class TragedyCalculator : IPerformanceCalculator
     }
     public decimal CalculateAmount()
     {
-        decimal performanceAmount = 40000;
-        if (_performance.Audience > 30)
+        if (!AudienceExceedVolumeThreshold())
         {
-            performanceAmount += 1000 * (_performance.Audience - 30);
+            return _baseAmount;
         }
-        return Convert.ToDecimal(performanceAmount / 100);
+        return _baseAmount + _extraAmountPerExceedAssistant * ExceededAudience();
+    }
+
+    private int ExceededAudience()
+    {
+        return _performance.Audience - _volumeThreshold;
+    }
+
+    private bool AudienceExceedVolumeThreshold()
+    {
+        return _performance.Audience > _volumeThreshold;
     }
 
     public int CalculateVolumeCredits()
