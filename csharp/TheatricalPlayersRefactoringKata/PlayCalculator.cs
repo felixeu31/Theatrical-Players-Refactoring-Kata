31cf4344eed23626496c707feb207322c0d570e2
Félix Díez <felixeu31@gmail.com>
2024-02-14 11:21:57 +0100

    refactor: extract base class PlayCalculator from Tragedy calculator

---
+++ csharp/TheatricalPlayersRefactoringKata/PlayCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata
+{
+    public class PlayCalculator : IPerformanceCalculator
+    {
+        private readonly Performance _performance;
+        private readonly int _volumeThreshold;
+        private readonly decimal _baseAmount;
+        private readonly int _extraAmountPerExceedAssistant;
+
+        public PlayCalculator(Performance performance, int volumeThreshold, decimal baseAmount, int extraAmountPerExceedAssistant)
+        {
+            _performance = performance;
+            _volumeThreshold = volumeThreshold;
+            _baseAmount = baseAmount;
+            _extraAmountPerExceedAssistant = extraAmountPerExceedAssistant;
+        }
+
+        protected int ExceededAudience()
+        {
+            return _performance.Audience - _volumeThreshold;
+        }
+
+        protected bool AudienceExceedVolumeThreshold()
+        {
+            return _performance.Audience > _volumeThreshold;
+        }
+
+        public int CalculateVolumeCredits()
+        {
+            var volumeCredits = Math.Max(_performance.Audience - 30, 0);
+            return volumeCredits;
+        }
+
+        public decimal CalculateAmount()
+        {
+            if (!AudienceExceedVolumeThreshold())
+            {
+                return _baseAmount;
+            }
+            return _baseAmount + _extraAmountPerExceedAssistant * ExceededAudience();
+        }
+    }
+}
--- csharp/TheatricalPlayersRefactoringKata/TragedyCalculator.cs
+++ csharp/TheatricalPlayersRefactoringKata/TragedyCalculator.cs
@@ -2,39 +2,15 @@
 
 namespace TheatricalPlayersRefactoringKata;
 
-public class TragedyCalculator : IPerformanceCalculator
+public class TragedyCalculator : PlayCalculator
 {
     private readonly Performance _performance;
     private readonly int _volumeThreshold = 30;
     private readonly decimal _baseAmount = 400;
     private readonly int _extraAmountPerExceedAssistant = 10;
 
-    public TragedyCalculator(Performance performance)
+    public TragedyCalculator(Performance performance) : base (performance, 30, 400, 10)
     {
         _performance = performance;
-    }
-    public decimal CalculateAmount()
-    {
-        if (!AudienceExceedVolumeThreshold())
-        {
-            return _baseAmount;
-        }
-        return _baseAmount + _extraAmountPerExceedAssistant * ExceededAudience();
-    }
-
-    private int ExceededAudience()
-    {
-        return _performance.Audience - _volumeThreshold;
-    }
-
-    private bool AudienceExceedVolumeThreshold()
-    {
-        return _performance.Audience > _volumeThreshold;
-    }
-
-    public int CalculateVolumeCredits()
-    {
-        var volumeCredits = Math.Max(_performance.Audience - 30, 0);
-        return volumeCredits;
     }
 }
