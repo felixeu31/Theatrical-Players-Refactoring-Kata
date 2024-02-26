deb89da0d53bf061ee4dc43acb2b86ef70190a5f
Félix Díez Arias <fdiez@tunstall.es>
2024-02-26 16:41:38 +0100

    refactor: move local variable to instance field

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -6,12 +6,18 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class StatementPrinter
     {
+        private readonly CultureInfo _cultureInfo;
+
+        public StatementPrinter()
+        {
+            _cultureInfo = new CultureInfo("en-US");
+        }
+
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
             var totalAmount = 0;
             var ownedCreditsByAudienceVolume = 0;
             var printedStatement = string.Format("Statement for {0}\n", invoice.Customer);
-            CultureInfo cultureInfo = new CultureInfo("en-US");
 
             foreach(var performance in invoice.Performances) 
             {   
@@ -25,10 +31,10 @@ namespace TheatricalPlayersRefactoringKata
                 if ("comedy" == play.Type) ownedCreditsByAudienceVolume += (int)Math.Floor((decimal)performance.Audience / 5);
 
                 // print line for this order
-                printedStatement += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(performanceAmount / 100), performance.Audience);
+                printedStatement += String.Format(_cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(performanceAmount / 100), performance.Audience);
                 totalAmount += performanceAmount;
             }
-            printedStatement += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+            printedStatement += String.Format(_cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             printedStatement += String.Format("You earned {0} credits\n", ownedCreditsByAudienceVolume);
             return printedStatement;
         }
